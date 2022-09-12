3e2b6eee275f270ec8635ea36bc5fbe2f42b14be
A-rielox <arielox.ag@gmail.com>
2022-09-12 19:38:25 -0300

    hasta 125 template forms

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+   credentials = { email: '', password: '' };
+   // con [(ngModel)]="credentials.email" PAL TWO WAY BINDING
+
+   showAlert = false;
+   alertMsg = 'Please wait! Your account is being logged.... 👍';
+   alertColor = 'blue';
+
    constructor() {}
 
    ngOnInit(): void {}
+
+   // no necesita q se le pase el event ya q ngSubmit para x defecto el refresh
+   login() {
+      this.showAlert = true;
+      this.alertMsg = 'Please wait! Your account is being logged.... 👍';
+      this.alertColor = 'blue';
+
+      console.log('user logged.... 👍');
+      console.log(this.credentials);
+   }
 }
